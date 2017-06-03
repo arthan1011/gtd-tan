427c7e52d94dbf9a2fa35a2e9cd1b32ff3c36cad
arthan1011 <arthan1011@gmail.com>
2017-06-03 11:38:05 +0300

    current and archive routes

--- src/main/webapp/gtd/app/router.js
+++ src/main/webapp/gtd/app/router.js
@@ -7,7 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('main');
+  this.route('main', function() {
+    this.route('current');
+    this.route('archive');
+  });
 });
 
 export default Router;
