8e93b00d4f77f8a64567ef8fdea096c6edf51dd3
arthan1011 <arthan1011@gmail.com>
2017-07-28 20:27:10 +0300

    front end forward filter

--- src/main/kotlin/org/arthan/kotlin/gtd/GtdTanApplication.kt
+++ src/main/kotlin/org/arthan/kotlin/gtd/GtdTanApplication.kt
@@ -2,8 +2,10 @@ package org.arthan.kotlin.gtd
 
 import com.fasterxml.jackson.databind.Module
 import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.arthan.kotlin.gtd.web.ForwardFilter
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.context.annotation.Bean
 
@@ -20,4 +22,14 @@ class GtdTanApplication {
 
     @Bean
     fun jacksonKotlinModule(): Module = KotlinModule()
+
+    @Bean
+    fun filterRegistrationBean(): FilterRegistrationBean {
+        val registration: FilterRegistrationBean = FilterRegistrationBean()
+        registration.filter = ForwardFilter()
+        registration.isEnabled = true
+        registration.addUrlPatterns("/")
+        registration.setName("forwardFilter")
+        return registration
+    }
 }
---
+++ src/main/kotlin/org/arthan/kotlin/gtd/web/ForwardFilter.kt
@@ -0,0 +1,27 @@
+package org.arthan.kotlin.gtd.web
+
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+
+/**
+ * Created by arthan on 28.07.2017. | Project gtd-tan
+ */
+
+class ForwardFilter : Filter {
+    override fun destroy() {
+
+    }
+
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain) {
+        println("through Forward filter!")
+        if (request is HttpServletRequest) {
+            request.getRequestDispatcher("/ui").forward(request, response)
+        } else {
+            chain.doFilter(request, response)
+        }
+    }
+
+    override fun init(filterConfig: FilterConfig?) {
+
+    }
+}
