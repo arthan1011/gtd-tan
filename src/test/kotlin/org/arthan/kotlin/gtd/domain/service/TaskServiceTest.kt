b7b05ff9cc7089bd41d0c04c3caa5bc3cfaddefb
shamsiev <shamsiev@iqmen.ru>
2017-08-29 14:37:13 +0300

    test profile fix

--- src/test/kotlin/org/arthan/kotlin/gtd/domain/service/TaskServiceTest.kt
+++ src/test/kotlin/org/arthan/kotlin/gtd/domain/service/TaskServiceTest.kt
@@ -12,6 +12,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import java.time.*
 import java.util.concurrent.atomic.AtomicBoolean
@@ -23,6 +24,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = arrayOf(GtdTanApplication::class))
+@ActiveProfiles(profiles = arrayOf("test"))
 class TaskServiceTest {
 
 	companion object {
